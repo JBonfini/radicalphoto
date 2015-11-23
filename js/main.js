9e340a4ab203ee79a4cd0684e7b44d04cc5a97e5
Jbonfini <justinbonfini@gmail.com>
2015-11-23 15:50:32 -0500

    Day 1
    Finished the index.html section and gallery.html section. Just have to
    add one more image to the gallery for some extra cash!

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// HOME SECTION
+
+var text_change = document.getElementById("text-change");
+
+function changeText() {
+    text_change.innerHTML = "Capturing Nature";
+    text_change.style.fontSize = "32px";
+    text_change.style.background = "#99C844";
+}
+
+setTimeout(changeText, 2500);
+
+// GALLERY SECTION
+
+/*var blue_dress = document.getElementById("blue-dress");
+var boxer = document.getElementById("boxer");
+var country_boy = document.getElementById("country-boy");
+var eye = document.getElementById("eye");
+var ice_swim = document.getElementById("ice-swim");
+var sitting = document.getElementById("sitting");
+var big_img = document.getElementById("big-img");
+
+blue_dress.onmouseover = function() {
+    big_img.src = "../images/blue_dress.jpg";
+}
+
+boxer.onmouseover = function() {
+    //big_img.src = "../images/boxer.jpg"; 
+    alert("testing");
+}*/
